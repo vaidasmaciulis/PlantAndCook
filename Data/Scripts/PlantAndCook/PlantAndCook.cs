2a167edb3c84b5146990d55507ba4886a8d5c9f5
DESKTOP-739BTMQ\Vaidas <vaidas.maciulis@gmail.com>
2021-11-14 21:29:26 +0200

    - Blinking effect have been removed

--- Data/Scripts/PlantAndCook/PlantAndCook.cs
+++ Data/Scripts/PlantAndCook/PlantAndCook.cs
@@ -1,62 +1,34 @@
-﻿using Sandbox.Game;
-using Sandbox.Game.Components;
+﻿using Sandbox.Game.Components;
 using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using VRage;
 using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.ModAPI;
 using VRage.ObjectBuilders;
 using VRage.Utils;
-using VRageMath;
 
 namespace PlantAndCook
 {
 	[MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
 	public class Session : MySessionComponentBase
 	{
-
-		public static ushort modId = 19008;
-		public static int runCount = 0;
-		public static Random rand = new Random();
-		public static List<long> blinkList = new List<long>();
-		public static int loadWait = 120;
-
-		protected override void UnloadData()
-		{
-			if (!MyAPIGateway.Multiplayer.IsServer)
-				MyAPIGateway.Multiplayer.UnregisterMessageHandler(modId, getPoke);
-		}
+		private static int runCount = 0;
 
 		public override void UpdateAfterSimulation()
 		{
 			try
 			{
-				// Only run every quarter of a second
-				if (++runCount % 15 > 0)
-					return;
-
-				// Blinking during load screen causes crash, don't load messagehandler on clients for 30s
-				if (!MyAPIGateway.Multiplayer.IsServer && loadWait > 0)
+				if (runCount++ < 100)
 				{
-					if (--loadWait == 0)
-						MyAPIGateway.Multiplayer.RegisterMessageHandler(modId, getPoke);
 					return;
 				}
-
-				foreach (var playerId in blinkList)
-					if (playerId == MyVisualScriptLogicProvider.GetLocalPlayerId())
-						blink(false);
-					else
-						MyAPIGateway.Multiplayer.SendMessageTo(modId, Encoding.ASCII.GetBytes("unblink"), MyVisualScriptLogicProvider.GetSteamId(playerId), true);
-				blinkList.Clear();
-
-				// Check for recharging or using toilet every second
-				if (runCount % 60 > 0)
-					return;
+				else
+				{
+					runCount = 0;
+				}
 
 				var players = new List<IMyPlayer>();
 				MyAPIGateway.Multiplayer.Players.GetPlayers(players, p => p.Character != null && p.Character.ToString().Contains("Astronaut"));
@@ -81,50 +53,14 @@ namespace PlantAndCook
 							if (food.Value > 0)
 								player.Character.GetInventory(0).AddItems((MyFixedPoint)0.05f, (MyObjectBuilder_PhysicalObject)MyObjectBuilderSerializer.CreateNewObject(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Organic")));
 						}
-
-					// Do remaining checks & stat updates every 5s
-					if (runCount < 100)
-						continue;
-
-					if (sleep.Value > 20 || rand.Next((int)sleep.Value) > 0)
-						continue;
-
-					blinkList.Add(player.IdentityId);
-					if (player.IdentityId == MyVisualScriptLogicProvider.GetLocalPlayerId())
-						blink(true);
-					else if (MyAPIGateway.Multiplayer.IsServer)
-						MyAPIGateway.Multiplayer.SendMessageTo(modId, Encoding.ASCII.GetBytes("blink"), MyVisualScriptLogicProvider.GetSteamId(player.IdentityId), true);
 				}
-
-				if (runCount > 299)
-					runCount = 0;
 			}
 			catch (Exception ex)
 			{
-				Echo("sleep exception", ex.ToString());
+				Echo("Plant and Cook exception", ex.ToString());
 			}
 		}
 
-		public void getPoke(byte[] poke)
-		{
-			// To call blink action on clients
-			try
-			{
-				var msg = ASCIIEncoding.ASCII.GetString(poke);
-				blink(msg == "blink");
-			}
-			catch (Exception ex)
-			{
-				Echo("sleep exception", ex.ToString());
-			}
-		}
-
-		public void blink(bool blink)
-		{
-			MyVisualScriptLogicProvider.ScreenColorFadingSetColor(Color.Black, 0L);
-			MyVisualScriptLogicProvider.ScreenColorFadingMinimalizeHUD(false);
-			MyVisualScriptLogicProvider.ScreenColorFadingStart(0.1f, blink, 0L);
-		}
 		private MyEntityStat GetPlayerStat(MyEntityStatComponent statComp, string statName)
 		{
 			MyEntityStat stat;
